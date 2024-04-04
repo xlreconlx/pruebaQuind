e50a7563acc03a2b79b851a8cef69c733ccc2f55
andersonur <andersonur@hotmail.com>
2024-04-04 12:27:08 -0500

    Configuracion inicial

---
+++ src/main/java/com/prueba/quind/QuindApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.quind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/quind/QuindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.quind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
